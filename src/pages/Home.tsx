723095549f2aab68037c700ad151d254c11a3ff9
surdarmaputra <surya.igede@gmail.com>
2023-08-21 12:26:52 +0700

    add responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,23 @@
 import { Button, Flex, Input } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { IconSearch } from '@tabler/icons-react';
 
 import userRepositoriesGroups from '../components/__mocks__/userRepositoriesGroups';
 import UserRepositoriesGroups from '../components/UserRepositoriesGroups';
 
 export default function Home() {
+  const largeScreen = useMediaQuery('(min-width: 480px)');
+
   return (
     <Flex
       align="center"
       direction="column"
       gap="md"
       justify="center"
+      m="0 auto"
       mih={50}
       p={16}
-      w="100%"
+      w={largeScreen ? '480px' : '100%'}
       wrap="wrap"
     >
       <Flex align="center" direction="column" gap="xs" mb="xs" w="100%">
